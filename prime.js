866185495442e55ef15f3586cade2e86c3f7b0cd
kavitasuthar123 <kavitasuthar.in@gmail.com>
2022-11-02 19:11:30 +0530

    prime code

--- prime.js
+++ prime.js
@@ -1,1 +1,15 @@
-console.log("kavita")
+function checkprime(num){
+    let factor=0;
+    for(let i=0;i<=num;i++){
+        if(num%i==1){
+            factor++
+        }
+    }
+    if(factor==i){
+        console.log("prime number");
+    }else {
+        console.log("not a prime number")
+    }
+}
+
+checkprime(13);
